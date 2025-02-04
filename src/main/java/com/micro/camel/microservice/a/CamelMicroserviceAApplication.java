6e02460a854bc495a0bf06aa9a98028a18160ef9
ITMASTER\MMF.Jan <mmf.jan@reative-almaviva.it>
2025-02-04 09:22:25 +0100

    first commit

---
+++ src/main/java/com/micro/camel/microservice/a/CamelMicroserviceAApplication.java
@@ -0,0 +1,13 @@
+package com.micro.camel.microservice.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMicroserviceAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelMicroserviceAApplication.class, args);
+	}
+
+}
