78f3f4246ded573789d5d879db0a20fa9d686efb
ITMASTER\MMF.Jan <mmf.jan@reative-almaviva.it>
2025-02-05 10:25:56 +0100

    file router added

---
+++ src/main/java/com/micro/camel/microservice/a/route/MyFileRouter.java
@@ -0,0 +1,14 @@
+package com.micro.camel.microservice.a.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFileRouter extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("file:files/input")
+                .log("${body}")
+        .to("file:files/output");
+    }
+}
--- src/main/java/com/micro/camel/microservice/a/route/MyFirstTimerRouter.java
+++ src/main/java/com/micro/camel/microservice/a/route/MyFirstTimerRouter.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component when its commented no route is created so we disable it
 public class MyFirstTimerRouter extends RouteBuilder {
     @Autowired
     private GetCurrentTimeBean getCurrentTimeBean;
@@ -20,9 +20,9 @@ public class MyFirstTimerRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        //queue...Timer
-        //transformation
-        //database....log
+        //queue...Timer       //input to read
+        //transformation      //data to process or transform
+        //database....log     //write to database(output)
         from("timer:first-timer")
                 //.transform().constant("Local time is : "+ LocalDateTime.now()) this time is constant
                 .bean(getCurrentTimeBean,"getCurrentTime") //here the time changes at each second i.e real time
